e25be623baff1b1eb9b65f9a4258792bb947c15c
aishwaryabolabattin <aishwaryabolabattin@gmail.com>
2026-05-01 11:56:39 +0530

    Add files via upload

---
+++ set_comprehension.py
@@ -0,0 +1,19 @@
+#set comprehension
+A={x for x in range(10)}
+print(A)
+
+B={x*x for x in range(5)}
+print(B)
+
+C={letter for letter in "Python"}
+print(C)
+
+D={x for x in range(2)}
+print(D)
+
+E={X for X in range(10) if X%2==0}
+print(E)
+
+F={X for X in range(20) if X%2!=0}
+print(F)
+
