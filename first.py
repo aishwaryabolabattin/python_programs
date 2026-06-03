852366c0ea4deb91d6c7870b850cf03be79f5894
aishwaryabolabattin <aishwaryabolabattin@gmail.com>
2026-06-03 11:11:23 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,12 @@
+#first file example using modules
+#user-defined modules
+def show():
+    return "This is my file"
+
+ 
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
---
+++ second.py
@@ -0,0 +1,35 @@
+#file2 example
+#import first
+#print(first.show())
+
+#import first 
+#print(first.sub(20,3))
+
+#import first
+#print(first.mul(5,10))
+
+#Built in Module
+import math
+print(math.ceil(2.3))
+
+import math
+print(math.sqrt(5))
+
+import math
+print(math.factorial(4))
+
+import math
+print(math.cbrt(4))
+
+import calendar
+print(calendar.month(2026,6))
+
+import calendar 
+print(calendar.weekday(2026,4,3))
+
+import keyword
+print(keyword.kwlist)
+
+import datetime
+print(datetime.date.today())
+
